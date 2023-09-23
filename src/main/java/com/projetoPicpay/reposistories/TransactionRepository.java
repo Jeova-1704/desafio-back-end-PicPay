f4e29ed4fa23c382178a0b8d09f947b9f058f196
Jeova BL <jeovab115@gmail.com>
2023-09-23 19:30:24 -0300

    Concluindo a entidade Transaction

--- src/main/java/com/projetoPicpay/reposistories/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetoPicpay.reposistories;
-
-import com.projetoPicpay.domain.transaction.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
-
-}
